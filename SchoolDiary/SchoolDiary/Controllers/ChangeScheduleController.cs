55bac2fca018b91147c14cc1f8c7e28d0a7bd60d
grs-ann <pv.forwork@gmail.com>
2021-03-22 12:20:48 +0400

    ChangeScheduleController has beed added.

---
+++ SchoolDiary/SchoolDiary/Controllers/ChangeScheduleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolDiary.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "admin")]
+    public class ChangeScheduleController : Controller
+    {
+        public ChangeScheduleController()
+        {
+
+        }
+        /*public IActionResult ChangeConcreteSchedule()
+        {
+
+        }*/
+    }
+}

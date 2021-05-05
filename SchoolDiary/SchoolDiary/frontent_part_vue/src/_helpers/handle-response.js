6ac9bb35fac358e75d587df466a76ba54d735ee4
grs-ann <pv.forwork@gmail.com>
2021-05-05 16:19:18 +0400

    A bug with incorrect parsing of request data was handled.

--- SchoolDiary/SchoolDiary/frontent_part_vue/src/_helpers/handle-response.js
+++ SchoolDiary/SchoolDiary/frontent_part_vue/src/_helpers/handle-response.js
@@ -2,21 +2,26 @@ import { authenticationService } from '@/_services';
 
 export function handleResponse(response) {
     return response.text().then(text => {
-        const data = text && JSON.parse(text);
-        if (!response.ok) {
-            if ([401, 403].indexOf(response.status) !== -1) {
-                // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-                authenticationService.logout();
-                location.reload(true);
-            }
-            if ([400].indexOf(response.status) !== -1) {
-                const error = data.errors;
+        try {
+            const data = text && JSON.parse(text);
+            if (!response.ok) {
+                if ([401, 403].indexOf(response.status) !== -1) {
+                    // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
+                    authenticationService.logout();
+                    location.reload(true);
+                }
+                // Обработка ошибок валидации моделей с бекенда.
+                if ([400].indexOf(response.status) !== -1 && data.title == "One or more validation errors occurred.") {
+                    const error = data.errors;
+                    return Promise.reject(error);
+                }
+                const error = (data && data.message) || response.statusText;
                 return Promise.reject(error);
             }
-            const error = (data && data.message) || response.statusText;
-            return Promise.reject(error);
-        }
 
-        return data;
+            return data;
+        } catch(err) {
+        }
+        
     });
 }

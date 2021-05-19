3495d87829695f5319d9bed342444715fa18eddb
grs-ann <pv.forwork@gmail.com>
2021-05-19 18:08:23 +0400

    Calendar date range picker has been added. Logic with sending date data to backend has been added.

---
+++ SchoolDiary/SchoolDiary/frontent_part_vue/src/teacher/Calendar.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <date-picker v-model="time" range></date-picker>
+    </div>
+</template>
+
+<script>
+import DatePicker from 'vue2-datepicker';
+import 'vue2-datepicker/index.css';
+
+export default {
+    components: {
+        DatePicker
+    },
+    data() {
+        return {
+            time: null
+        }
+    },
+    created() {
+        this.getInitialDataForCalendar();
+    },
+    watch: {
+        time(newV) {
+            this.getTime();
+        }
+    },
+    methods: {
+        getTime() {
+            this.firstDate = this.time[0];
+            this.lastDate = this.time[1];
+            this.$emit('dateChange', this.time);
+        },
+        getInitialDataForCalendar() {
+            let now = new Date();
+            let day = now.getDate();
+            let month = now.getMonth();
+            let year = now.getFullYear();
+            let result = day + '/' + month + '/' + year;
+            //this.initial = result;
+        }
+    }
+}
+</script>
--- SchoolDiary/SchoolDiary/frontent_part_vue/src/teacher/ConcreteClassWithStudents.vue
+++ SchoolDiary/SchoolDiary/frontent_part_vue/src/teacher/ConcreteClassWithStudents.vue
@@ -1,32 +1,56 @@
 <template>
     <div>
-        <div>
-            <select v-model="selectedSubject">
-                <option v-for="(subject, index) in pinnedSubjects" v-bind:value="subject">{{ subject.title }}</option>
-            </select>
+        <div v-bind:style="styles">
+            <div>
+                <p  v-show="clientValidations.dateErrorMessage">{{ clientValidations.date }}</p>
+                <Calendar
+                    @dateChange="getChangedDate"
+                />
+            </div>
+            <div>
+                <p v-show="clientValidations.subjectErrorMessage">{{ clientValidations.subject }}</p>
+                <select v-model="selectedSubject">
+                    <option v-for="(subject, index) in pinnedSubjects" v-bind:value="subject">{{ subject.title }}</option>
+                </select>
+            </div>
         </div>
-        {{ selectedSubject }}
         <div v-for="(cl, index) in this.concreteClassData.students">
             {{ index + 1 }})
             {{ cl.user.lastname }}
             {{ cl.user.firstname }}
             <button @click="showMarks(cl.id)">Показать оценки</button>
         </div>
+        
     </div>
 </template>
 
 <script>
 import { teacherManagementService, authenticationService } from "@/_services";
 import axios from 'axios';
+import Calendar from './Calendar';
 
 
 export default {
+    components: {
+        Calendar
+    },
     data() {
         return {
             students: [],
-            selectedSubject: {},
+            selectedSubject: null,
+            studentMarks: {},
+            firstDate: null,
+            lastDate: null,
 
-            studentMarks: {}
+            clientValidations: {
+                date: 'Необходимо выбрад период дат!',
+                dateErrorMessage: false,
+                subject: 'Необходимо выбрать предмет!',
+                subjectErrorMessage: false
+            },
+            styles: {
+                'color': 'red'
+            }
         }
     },
     props: {
@@ -37,10 +61,23 @@ export default {
             type: Array
         }
     },
-    created() {
-    },
     methods: {
         showMarks(studentId) {
+            // todo: когда будет время, переписать это на нормальный лад.
+            if (this.selectedSubject == null) {
+                this.clientValidations.subjectErrorMessage = true;
+            } else {
+                this.clientValidations.subjectErrorMessage = false;
+            }
+            if (this.firstDate == null || this.lastDate == null) {
+                this.clientValidations.dateErrorMessage = true;
+            } else {
+                this.clientValidations.dateErrorMessage = false;
+            }
+            if (this.clientValidations.dateErrorMessage == true ||
+                this.clientValidations.subjectErrorMessage == true) {
+                return;
+            }
             const currentUser = authenticationService.currentUserValue || {};
             const authHeader = currentUser.token ? { 'Authorization': 'Bearer ' + currentUser.token } : {}
 
@@ -48,7 +85,9 @@ export default {
             axios.get('https://localhost:44303/api/Teacher/GetStudentMarks', {
                 params: {
                     studentId: studentId,
-                    subjectId: this.selectedSubject.id
+                    subjectId: this.selectedSubject.id,
+                    firstDate: this.firstDate,
+                    lastDate: this.lastDate
                 },
                 headers: {
                     ...authHeader
@@ -57,6 +96,10 @@ export default {
             .then(response => {
                 console.log(response.data)
             })},
+        getChangedDate(data) {
+            this.firstDate = data[0];
+            this.lastDate = data[1];
+        }
     }   
 }
 </script>
